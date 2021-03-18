5f6ae559b6265e6b8f99b96f64f3f854f33e81bc
Nauman Umer <nmanumr@gmail.com>
2021-03-19 00:20:37 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 $(function () {
-  $("#myForm").submit(handleFormSubmit);
+ $('#myForm').validate({
+  rules: {
+    name: {
+      required: true,
+      minlength: 5,
+    },
+    email: {
+      required: true,
+      email: true,
+    },
+    password: {
+      required: true,
+      minlength: 8,
+    },
+    password2: {
+      minlength: 8,
+      equalTo : "#password"
+    },
+  },
+  submitHandler: function (form) { // for demo
+    alert('valid form submitted'); // for demo
+    return false; // for demo
+  }
 });
-
-function handleFormSubmit(e) {
-  alert("Form is being submitted");
-
-  e.preventDefault();
-}
+});
